74871c46e6d1ed5f7f3e10d6e42e873afe12d41b
AriandaRiski <mriskiariandaa@gmail.com>
2023-07-06 00:37:43 +0700

    next auth credentials

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,14 +1,17 @@
 import "../../styles/main.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react"
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps : { session, ...pageProps }}) {
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <Component {...pageProps} />
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
         </>
     )
 }
