1547b0399f5769989bbdd8f20495f11aadaf35e1
madeby.ariand <92702048+AriandaRiski@users.noreply.github.com>
2023-07-15 09:57:24 +0700

    menampilkan data menggunakan react-datatable-component (#3)

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -3,6 +3,8 @@ import { useSession, signOut } from 'next-auth/react'
 
 const Header = () => {
 
+    const { data: session } = useSession();
+
     return (
         <>
             <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
@@ -13,7 +15,14 @@ const Header = () => {
                 <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
                 <div className="navbar-nav">
                     <div className="nav-item text-nowrap">
-                        <a className="nav-link px-3" type='button' onClick={() => signOut({ callbackUrl: '/login' })}>Sign out</a>
+                        <div className="btn-group">
+                            <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                {session?.name}
+                            </button>
+                            <ul className="dropdown-menu">
+                                <li><a type='button' className="dropdown-item" onClick={() => signOut({ callbackUrl: '/login' })}>Sign out</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </header>
