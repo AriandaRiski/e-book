58b1dcb6ce69775e978a6ca1f44d4fb052f8fc68
AriandaRiski <mriskiariandaa@gmail.com>
2023-07-15 09:56:21 +0700

    menampilkan data menggunakan react-datatable-component

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,17 +4,27 @@ import Head from "next/head";
 import { SessionProvider } from "next-auth/react"
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { StyleSheetManager } from 'styled-components';
+import isPropValid from '@emotion/is-prop-valid';
+import { useEffect } from "react";
 
-export default function MyApp({ Component, pageProps : { session, ...pageProps }}) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+    
     return (
         <>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <SessionProvider session={session}>
-                <Component {...pageProps} />
-                <ToastContainer/>
-            </SessionProvider>
+            <StyleSheetManager shouldForwardProp={isPropValid}>
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </Head>
+                <SessionProvider session={session}>
+                    <Component {...pageProps} />
+                    <ToastContainer />
+                </SessionProvider>
+            </StyleSheetManager>
         </>
     )
 }
--- frontend/src/pages/admin/kategori/list.jsx
+++ frontend/src/pages/admin/kategori/list.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Layout from '@/components/layout/layout'
 import { useSession } from 'next-auth/react'
+import DataTable from 'react-data-table-component';
 
 const list = () => {
     const { data: session } = useSession();
-    const [kategori, setKategori] = useState(null);
+    const [kategori, setKategori] = useState([]);
+    const [pending, setPending] = useState(true);
 
     useEffect(() => {
         if (session) {
@@ -23,7 +25,11 @@ const list = () => {
                     const getData = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/kategori/list`, requestOptions);
                     const response = await getData.json()
 
-                    setKategori(response.data)
+                    const timeout = setTimeout(() => {
+                        setKategori(response.data)
+                        setPending(false);
+                    }, 700);
+                    return () => clearTimeout(timeout);
 
                 } catch (error) {
                     console.log(error)
@@ -31,46 +37,106 @@ const list = () => {
 
             })();
         }
-    }, [session])
+    }, [session]);
+
+    const columns = [
+        {
+            name: '#',
+            selector: (row, index) => index + 1,
+            width: '50px'
+        },
+        {
+            name: 'Kategori',
+            selector: row => row.kategori,
+            sortable: true,
+        },
+        {
+            name: 'Aksi',
+            cell: () => {
+                return (
+                    <>
+                        <button type='button' className='m-2 btn btn-warning btn-sm' data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
+                        <button className='m-2 btn btn-danger btn-sm'>Hapus</button>
+                    </>
+                )
+            },
+            width: '140px',
+            ignoreRowClick: true,
+            allowOverflow: true,
+            button: true,
+        },
+    ];
+
+    const paginationComponentOptions = {
+        rowsPerPageText: 'Jumlah Per Halaman',
+        rangeSeparatorText: 'dari',
+        selectAllRowsItem: true,
+        selectAllRowsItemText: 'Semua ',
+    };
+
+    const action = (<button className='btn btn-primary btn-sm' data-bs-toggle="modal" data-bs-target="#tambahModal">Tambah</button>);
 
     return (
         <>
             <Layout>
                 <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 className="h2">List Kategori</h1>
-                    <div className="btn-toolbar mb-2 mb-md-0">halo, {session?.user.name}</div>
                 </div>
+                <br/>
                 <div className="table-responsive">
-                    <div className=''>
-                        <button type="button" className="btn-sm btn btn-primary">Tambah</button>
-                    </div>
-
-                    <table className="table table-borderless">
-                        <thead>
-                            <tr>
-                                <th className="text-center" width="5%" scope="col">No</th>
-                                <th scope="col">Nama Kategori</th>
-                                <th className="text-center" width="20%" scope="col">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                kategori && kategori.map((data, index) =>
-                                    <tr>
-                                        <td className="text-center">{index + 1}</td>
-                                        <td>{data.kategori}</td>
-                                        <td>
-                                            <div className="text-center">
-                                                <button type="button" className="m-2 btn-sm btn btn-warning">Edit</button>
-                                                <button type="button" className="m-2 btn-sm btn btn-danger">Hapus</button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
+                    <DataTable
+                        columns={columns}
+                        data={kategori}
+                        actions={action}
+                        paginationComponentOptions={paginationComponentOptions}
+                        paginationPerPage={10}
+                        progressPending={pending}
+                        pagination
+                    />
                 </div>
+
+                {/* Modal Tambah */}
+                <div>
+                    <div className="modal fade" id="tambahModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Tambah Kategori</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                                </div>
+                                <div className="modal-body">
+                                    ...
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary">Simpan</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Modal Edit */}
+                <div>
+                    <div className="modal fade" id="editModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Edit Kategori</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                                </div>
+                                <div className="modal-body">
+                                    ...
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary">Simpan</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </Layout >
         </>
     )
