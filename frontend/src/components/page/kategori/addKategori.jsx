4b730e6812267aefd97bb575fbfc432863fa2814
AriandaRiski <mriskiariandaa@gmail.com>
2023-07-24 14:45:19 +0700

    membuat edit kategori

---
+++ frontend/src/components/page/kategori/addKategori.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
+import { addKategori, editKategori } from '@/redux/action/kategori';
+import { useSession } from 'next-auth/react';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '@/redux/action/modalDialog';
+import { toast } from 'react-toastify';
+import { useState } from 'react';
+
+const addForm = (row) => {
+
+    const { data: session } = useSession()
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (values) => {
+
+        try {
+
+            if (row.action) {
+
+                const value = {
+                    id_kategori: row.data.id_kategori,
+                    kategori: values.kategori,
+                }
+
+                const edit = await dispatch(editKategori({ token: session.tokenAccess, values: value }));
+                const response = edit.payload;
+
+                if (!response.success) {
+                    alert(response.message)
+                } else {
+                    toast.success(response.message, {
+                        position: "top-center",
+                        autoClose: 1000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                }
+
+            } else {
+
+                const tambah = await dispatch(addKategori({ token: session.tokenAccess, values: values }));
+                const response = tambah.payload;
+
+                if (!response.success) {
+                    alert(response.message)
+                } else {
+                    toast.success(response.message, {
+                        position: "top-center",
+                        autoClose: 1000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                }
+            }
+
+            dispatch(closeModal())
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            kategori: row.data ? row.data.kategori : ''
+        },
+
+        validationSchema: Yup.object({
+            kategori: Yup.string().required()
+        }),
+
+        // onSubmit: row.action ? handleEdit : handleSubmit
+        onSubmit: handleSubmit
+
+    })
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div className="modal-body">
+                <div className="form-floating">
+                    <input type="text" className="form-control" name="kategori" placeholder="masukkan nama kategori"
+                        {...formik.getFieldProps('kategori')}
+                    />
+                    <label htmlFor="floatingInput">Kategori</label>
+                    {formik.errors.kategori && <div className='error'>{formik.errors.kategori}</div>}
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => dispatch(closeModal())}>Close</button>
+                <button type="submit" className="btn btn-primary">Simpan</button>
+            </div>
+        </form>
+    );
+};
+
+export default addForm;
--- frontend/src/pages/admin/kategori/test.jsx
+++ frontend/src/pages/admin/kategori/test.jsx
@@ -3,6 +3,42 @@ import Button from 'react-bootstrap/Button';
 import { useDispatch } from 'react-redux';
 import { openModal } from '@/redux/action/modalDialog';
 import Form from 'react-bootstrap/Form';
+import AddForm from '@/components/page/kategori/addKategori';
+
+// sweet alert
+// import Swal from 'sweetalert2';
+
+// const feedBackNotif = (icon, title, message) => {
+//     Swal.fire({
+//         title: title,
+//         text: message,
+//         icon: icon,
+//         timer: 2000,
+//         showConfirmButton: false,
+//         allowOutsideClick: true
+//     })
+// }
+
+// Swal.fire({
+//     title: 'Are you sure?',
+//     text: "You won't be able to revert this!",
+//     icon: 'warning',
+//     showCancelButton: true,
+//     confirmButtonColor: '#3085d6',
+//     cancelButtonColor: '#d33',
+//     confirmButtonText: 'Yes, delete it!'
+// }).then(async (result) => {
+//     if (result.isConfirmed) {
+//         const hapus = await dispatch(deleteKategori({ token: session.tokenAccess, id: id }));
+//         const response = hapus.payload;
+//         if (response.success == false) {
+//             return feedBackNotif('error', 'Error!', response.message)
+//         } else if (kategori.isDelete == true) {
+//             return feedBackNotif('success', 'Berhasil!', 'oke')
+//         }
+//     }
+// })
+
 
 
 function FormTextExample() {
@@ -29,7 +65,8 @@ const test = () => {
     return (
         <div>
             <Button variant="primary" onClick={() => { dispatch(openModal({ title: 'modal1', content: "isinya" })) }}>Modal 1</Button>
-            <Button variant="primary" onClick={() => { dispatch(openModal({ title: 'modal2', content: <FormTextExample /> })) }}>Modal 2</Button>
+            <Button variant="success" onClick={() => { dispatch(openModal({ title: 'modal2', content: <FormTextExample /> })) }}>Modal 2</Button>
+            <Button variant="info" onClick={() => { dispatch(openModal({ title: 'Modal Test', content: <AddForm /> })) }}>Modal test</Button>
         </div>
     )
 }
